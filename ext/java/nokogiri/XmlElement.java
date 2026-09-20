63e59e25291de917e5bd6637cd72a43ae1f9dc2b
Yoko Harada <yokolet@gmail.com>
2026-09-21 00:18:31 +0900

    implementing html_standard_serialze method

--- ext/java/nokogiri/XmlElement.java
+++ ext/java/nokogiri/XmlElement.java
@@ -1,9 +1,12 @@
 package nokogiri;
 
+import nokogiri.internals.NokogiriHelpers;
 import org.jruby.Ruby;
 import org.jruby.RubyClass;
 import org.jruby.anno.JRubyClass;
+import org.jruby.anno.JRubyMethod;
 import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.builtin.IRubyObject;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
@@ -41,5 +44,22 @@ public class XmlElement extends XmlNode
     visitor.enter((Element) node);
     acceptChildren(context, getChildren(), visitor);
     visitor.leave((Element) node);
+  }
+
+  // This method is added to handle write_to method of HTML5::Node
+  @JRubyMethod(name = "html_standard_serialize")
+  public IRubyObject html_standard_serialize(ThreadContext context, IRubyObject option)
+  {
+    Boolean preserve_newline = option.toJava(Boolean.class);
+    String output = null;
+    if (getNode() instanceof nokogiri.internals.html5.nodes.Node) {
+      nokogiri.internals.html5.nodes.Node innerNode = (nokogiri.internals.html5.nodes.Node) getNode();
+      nokogiri.internals.html5.nodes.Document innerDoc = (nokogiri.internals.html5.nodes.Document) innerNode.getOwnerDocument();
+      innerDoc.outputSettings().prettyPrint(preserve_newline);
+      output = innerNode.outerHtml();
+    } else {
+      getNode().toString();
+    }
+    return NokogiriHelpers.stringOrBlank(context.runtime, output);
   }
 }
